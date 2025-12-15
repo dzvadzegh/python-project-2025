711f80338245bc44a124df5124f377605b8cf62d
LAZhelezova <lidia.zhelezova@mail.ru>
2025-12-15 16:10:50 +0300

    Добавлена проверка исключений, прописано поведение в случае отсутствия пользователя в БД

--- bot/commands/info.py
+++ bot/commands/info.py
@@ -2,27 +2,34 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
-router = Router()
+info_router = Router()
 
 
-@router.message(Command("info"))
+@info_router.message(Command("info"))
 async def bot_info(message: Message):
-    db = message.bot["db"]
-    user_id = message.from_user.id
-    user_data = await db.get_user(user_id)
+    try:
+        db = message.bot["db"]
+        user_id = message.from_user.id
+        user_data = await db.get_user(user_id)
+        if user_data is None:
+            await message.answer("❌ Нет информации о пользователе ❌")
+            return
 
-    settings = user_data.get("settings", {}) if user_data.get("settings") else {}
-    notification_time = settings.get("notification_time", "10:00")
-    reminders_per_day = settings.get("reminders_per_day", 1)
-    timezone = settings.get("timezone", "UTC")
-    language = settings.get("language", "рус")
+        settings = user_data.get("settings", {}) if user_data else {}
+        notification_time = settings.get("notification_time", "10:00")
+        reminders_per_day = settings.get("reminders_per_day", 1)
+        timezone = settings.get("timezone", "UTC")
+        language = settings.get("language", "рус")
 
-    info_message = (
-        f"⚙️ *Информация о настройках*\n\n"
-        f"Время уведомлений: {notification_time}\n"
-        f"Уведомлений в день: {reminders_per_day}\n"
-        f"Временная зона: {timezone}\n"
-        f"Язык: {language}\n"
-        f"Посмотреть статистику: /stats"
-    )
-    await message.answer(text=info_message, parse_mode="Markdown")
+        info_message = (
+            f"⚙️ *Информация о настройках*\n\n"
+            f"Время уведомлений: {notification_time}\n"
+            f"Уведомлений в день: {reminders_per_day}\n"
+            f"Временная зона: {timezone}\n"
+            f"Язык: {language}\n"
+            f"Посмотреть статистику: /stats"
+        )
+        await message.answer(text=info_message, parse_mode="Markdown")
+
+    except Exception:
+        await message.answer("Ошибка подключения к базе данных")
